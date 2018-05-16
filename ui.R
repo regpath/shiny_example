24a1fa795ce2336e76fa4eed70349ca9110da110
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 11:30:54 +0900

    Update R file(s)

--- ui.R
+++ ui.R
@@ -1,13 +1,10 @@
 pageWithSidebar(
-  headerPanel('Iris k-means clustering'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-                selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 9)
+    numericInput("initial", "Initial stock", 20),
+    numericInput("lambda", "Average daily failure count", 0.33)
   ),
   mainPanel(
-    plotOutput('plot1')
+    plotOutput('plot1'),
+	plotOutput('plot2')
   )
 )
