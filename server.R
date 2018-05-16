4ea85d60f8c455a856bff3c1032b7c3885fc9a82
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 11:34:49 +0900

    Update R file(s)

--- server.R
+++ server.R
@@ -1,7 +1,10 @@
 function(input, output) {
+
+	Stock_level = reactive(input$initial)
+	Days_Failure_Count = reactive(rpois(365,input$lambda))
+	
 	output$plot1 <- renderPlot({
-		# plot(Days_Failure_Count())
-		plot(c(1,2,3,4,5))
+		plot(Days_Failure_Count())
 		})
 	output$plot2 <- renderPlot({
 		# plot(Inventory_Level())
