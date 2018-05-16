a5b1ccfb2767b2783042f7d7e21452eca299cb52
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 11:24:25 +0900

    Update R file(s)

--- server.R
+++ server.R
@@ -5,11 +5,11 @@ function(input, output) {
 
 	output$plot1 <- renderPlot({
 		# plot(Days_Failure_Count())
-		plot(c(1,2,3,4,5)
+		plot(c(1,2,3,4,5))
 		})
 	output$plot2 <- renderPlot({
 		# plot(Inventory_Level())
-		plot(c(1,2,3,4,5)
+		plot(c(1,2,3,4,5))
 		})	
 
 }
