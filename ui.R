f3ee6e08bcd87211faefcfd06d98f99b1532f105
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 10:56:28 +0900

    Update R file(s)

--- ui.R
+++ ui.R
@@ -10,10 +10,10 @@ fluidPage(
       numericInput("lambda", "Average daily failure count", 0.33),
 
       # Output: Header + summary of distribution ----
-      h4("Daily Failure Count"),
-      plotOutput("plot1"),
+    #  h4("Daily Failure Count"),
+    #  plotOutput("plot1"),
 
       # Output: Header + table of distribution ----
-      h4("Daily Inventory Level"),
+    #  h4("Daily Inventory Level"),
     #  plotOutput("plot2")
     )
