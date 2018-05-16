45517e33116d6c7dd803754796545d814e8655f7
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 13:26:36 +0900

    Update R file(s)

--- server.R
+++ server.R
@@ -3,12 +3,24 @@ function(input, output) {
 	Stock_level = reactive(input$initial)
 	Days_Failure_Count = reactive(rpois(365,input$lambda))
 	
+	Replenish_Level = rep(0,365)
+	Replenish_Level[35:365] = Days_Failure_Count[1:(365-34)]
+
+	Inventory_Level_st = Inventory_Level_ed = vector()
+	Inventory_Level_st[1]=Stock_level
+	Inventory_Level_ed[1]=Inventory_Level_st[1] - Days_Failure_Count[1] + Replenish_Level[1]
+	for (i in 2:365) {
+		Inventory_Level_st[i]=Inventory_Level_ed[i-1];
+		Inventory_Level_ed[i]=Inventory_Level_st[i] - Days_Failure_Count[i] + Replenish_Level[i]
+	}
+	
+	
 	output$plot1 <- renderPlot({
 		plot(Days_Failure_Count())
 		})
 	output$plot2 <- renderPlot({
-		# plot(Inventory_Level())
-		plot(c(1,2,3,4,5))
+		plot(Inventory_Level_ed)
+		# plot(c(1,2,3,4,5))
 		})	
 
 }
