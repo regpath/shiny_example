401af68ca99ea50b74d7f8e52dc9cbd2d41f4190
Jeongyong Kim <jeongyong.kim@sk.com>
2018-05-16 16:59:40 +0900

    revision

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ pageWithSidebar(
 	mainPanel(
 		plotOutput('plot1'),
 		plotOutput('plot2'),
-		dataTableOutput('table'),
+		dataTableOutput('table1'),
 		textOutput('text1'),
 		textOutput('text2'),
 		textOutput('text3'),
